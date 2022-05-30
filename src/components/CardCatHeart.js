138d1d26310697bf4d8533085eba7d806f1d240b
Morning-Shine <maskacat@gmail.com>
2022-05-30 13:48:20 +0300

    v1.0 changed IMG-paths

--- src/components/CardCatHeart.js
+++ src/components/CardCatHeart.js
@@ -2,31 +2,36 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addFavoriteCat, removeFavoriteCat } from '../store/favoriteCatsSlice';
 import styled from "@emotion/styled";
+import emptyHeart from '../assets/img/heart.png';
+import hoveredHeart from '../assets/img/heart_hovered.png';
+import clickedHeart from '../assets/img/heart_clicked.png';
+
 
 
 export default function CardCatHeart({ visible, item }) {
     const dispatch = useDispatch();
     const isFavorite = useSelector(state => state.favoriteCats.cats.find(cat => cat.id === item.id));
-    const [heart, setHeart] = useState(isFavorite ? '/assets/img/heart_clicked.png' : '/assets/img/heart.png');
+    const [heart, setHeart] = useState(isFavorite ? clickedHeart : emptyHeart);
 
     return (
         <Heart
             hidden={visible}
-            onMouseOver={isFavorite ? null : () => setHeart('/assets/img/heart_hovered.png')}
-            onMouseLeave={isFavorite ? null : () => setHeart('/assets/img/heart.png')}
+            onMouseOver={isFavorite ? null : () => setHeart(hoveredHeart)}
+            onMouseLeave={isFavorite ? null : () => setHeart(emptyHeart)}
             onClick={
                 isFavorite
                     ? () => {
                         dispatch(removeFavoriteCat({ item }));
-                        setHeart('/assets/img/heart.png')
+                        setHeart(emptyHeart)
                     }
 
-                    : () => { dispatch(addFavoriteCat({ item })); 
-                        setHeart('/assets/img/heart_clicked.png')
-                }
-            } 
+                    : () => {
+                        dispatch(addFavoriteCat({ item }));
+                        setHeart(clickedHeart)
+                    }
+            }
         >
-    <Img src={heart} />
+            <Img src={heart} />
         </Heart >
     )
 }
