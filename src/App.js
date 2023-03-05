7ab11433b9eff6975412b88badd046f0f7e9f03f
Morning-Shine <maskacat@gmail.com>
2023-03-05 13:12:13 +0300

    v1.5 updated js-guid, gh-pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import AppBar from "./components/AppBar";
-import AppRouter from "./components/AppRouter";
+import React from 'react';
+import AppBar from './components/AppBar';
+import AppRouter from './components/AppRouter';
 import { useSelector } from 'react-redux';
-import ViewCat from "./components/ViewCat";
+import ViewCat from './components/ViewCat';
 import './App.css';
 
-
 function App() {
-  const { url, isHide } = useSelector(state => state.viewCat);
+  const { url, isHide } = useSelector((state) => state.viewCat);
 
   return (
     <div className="App">
       <AppBar />
       <AppRouter />
-      {url
-        ? <ViewCat isHide={isHide} url={url} />
-        : null
-      }
+      {url ? <ViewCat isHide={isHide} url={url} /> : null}
     </div>
   );
 }
